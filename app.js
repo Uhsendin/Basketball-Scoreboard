1f699f35d645992205c9beca36750288d44596b5
Uhsendin <chilebr99@gmail.com>
2022-11-07 18:56:28 -0500

    Added Guest scoreboard

--- app.js
+++ app.js
@@ -6,5 +6,40 @@ function plusOne() {
   num++;
   num.toString();
   homePoints.textContent = num;
-  console.log((homePoints.textContent = num));
+ 
+}
+
+function plusTwo() {
+    let num = parseInt(homePoints.textContent)
+    num += 2;
+    num.toString();
+    homePoints.textContent = num
+}
+
+function plusThree() {
+  let num = parseInt(homePoints.textContent)
+  num += 3;
+  num.toString();
+  homePoints.textContent = num
+}
+
+function plusOneGuest() {
+  let num = parseInt(guestPoints.textContent);
+  num++;
+  num.toString();
+  guestPoints.textContent = num
+}
+
+function plusTwoGuest() {
+  let num = parseInt(guestPoints.textContent);
+  num += 2;
+  num.toString();
+  guestPoints.textContent = num
+}
+
+function plusThreeGuest() {
+  let num = parseInt(guestPoints.textContent)
+  num += 3;
+  num.toString();
+  guestPoints.textContent = num
 }
