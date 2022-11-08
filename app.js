db7f891e6b7d49f482ffd810e59aefd8f5579507
Uhsendin <chilebr99@gmail.com>
2022-11-07 19:43:29 -0500

    Removed repeated code

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
 let homePoints = document.getElementById("home-points");
 let guestPoints = document.getElementById("guest-points");
 
-function plusOne() {
-  let num = parseInt(homePoints.textContent);
-  num++;
-  num.toString();
-  homePoints.textContent = num;
- 
-}
-
-function plusTwo() {
-    let num = parseInt(homePoints.textContent)
-    num += 2;
+function addScore(value, side) {
+  if (side == "home") {
+    let num = parseInt(homePoints.textContent);
+    num += value
     num.toString();
-    homePoints.textContent = num
-}
-
-function plusThree() {
-  let num = parseInt(homePoints.textContent)
-  num += 3;
-  num.toString();
-  homePoints.textContent = num
-}
-
-function plusOneGuest() {
-  let num = parseInt(guestPoints.textContent);
-  num++;
-  num.toString();
-  guestPoints.textContent = num
-}
-
-function plusTwoGuest() {
-  let num = parseInt(guestPoints.textContent);
-  num += 2;
-  num.toString();
-  guestPoints.textContent = num
-}
-
-function plusThreeGuest() {
-  let num = parseInt(guestPoints.textContent)
-  num += 3;
-  num.toString();
-  guestPoints.textContent = num
+    homePoints.textContent = num;
+  } else {
+    let num = parseInt(guestPoints.textContent);
+    num += value
+    num.toString();
+    guestPoints.textContent = num;
+  }
 }
